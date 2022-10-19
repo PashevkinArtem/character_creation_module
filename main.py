f24083c18ea35199642faac7d165c74ccda96900
PashevkinArtem <artem.pashevkin@yandex.com>
2022-10-19 23:47:12 +0300

    code according to PEP8

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def attack(char_name, char_class):
     if char_class == 'healer':
         return (f'{char_name} нанёс противнику урон, равный '
                 f'{5 + randint(-3, -1)}')
+    return (f'{char_name} не применил специальное умение')
 
 
 def defence(char_name, char_class):
@@ -20,16 +21,18 @@ def defence(char_name, char_class):
         return (f'{char_name} блокировал {10 + randint(-2, 2)} ед. урона')
     if char_class == 'healer':
         return (f'{char_name} блокировал {10 + randint(2, 5)} ед. урона')
+    return (f'{char_name} не применил специальное умение')
 
 
 def special(char_name, char_class):
     if char_class == 'warrior':
-        return (f'{char_name} применил специальное умение '
+        return (f'{char_name} применил специssальное умение '
                 f'«Выносливость {80 + 25}»')
     if char_class == 'mage':
         return (f'{char_name} применил специальное умение «Атака {5 + 40}»')
     if char_class == 'healer':
         return (f'{char_name} применил специальное умение «Защита {10 + 30}»')
+    return (f'{char_name} не применил специальное умение')
 
 
 def start_training(char_name, char_class):
